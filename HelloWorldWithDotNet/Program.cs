e3e14bc3c280f9df098430a167faa9c19f302377
Sakshee Dubey <dbysakshee@gmail.com>
2021-03-14 17:45:13 +0530

    Initial Commit

---
+++ HelloWorldWithDotNet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldWithDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World With Dot Net");
+            Console.ReadKey();
+        }
+    }
+}
